c0e949a1b215b8390cab462596f3467485005615
Михаил <migel.bd@gmail.com>
2021-04-23 16:27:36 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@
 from setuptools import setup, find_packages
 from pact_im import __version__
 
+with open('README.md', 'r', encoding='utf-8') as readme:
+    long_description = readme.read()
+
 setup(
     name='pact_im',
+    keywords='library,pact',
+    long_description=long_description,
     version=__version__,
     description='PactIM Python API',
-    author='Badrazhan Mikhail',
+    author='Pact LLC',
     packages=find_packages(),
     requires=[
         'requests',
